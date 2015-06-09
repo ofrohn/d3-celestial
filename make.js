07b2b48c0e42bc8c83fc04d4999ef4c6fbd36736
ofrohn <ol.frohn@gmail.com>
2015-06-09 12:51:08 +0200

    Change path

--- make.js
+++ make.js
@@ -8,7 +8,7 @@ var shell = require('shelljs/make'),
     begin = "!(function() {",
     end = "this.Celestial = Celestial;\n})();",
     filename = './celestial',
-    FINAL = true;
+    FINAL = false;
 
     
 target.all = function() {
