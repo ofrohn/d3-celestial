ad9814f9e3c2c469aebe8165e4de043a7e5459a6
ofrohn <ofroh@web.de>
2020-05-29 22:29:42 +0200

    svg custom symbol error

--- celestial.js
+++ celestial.js
@@ -5049,81 +5049,81 @@ function saveSVG(fname) {
 
 }
 
-  var customSvgSymbols = d3.map({
-    'ellipse': function(size, ratio) {
-      var s = Math.sqrt(size), 
-          rx = s*0.666, ry = s/3;
-      return 'M' + (-rx) + ',' + (-ry) +
-      ' m' + (-rx) + ',0' +
-      ' a' + rx + ',' + ry + ' 0 1,0' + (rx * 2) + ',0' +
-      ' a' + rx + ',' + ry + ' 0 1,0' + (-(rx * 2)) + ',0';
-    },
-    'marker': function(size, ratio) {
-      var s =  size > 48 ? size / 4 : 12,
-          r = s/2, l = r-3;
-      return 'M ' + (-r) + ' 0 h ' + l + 
-             ' M 0 ' + (-r) + ' v ' + l + 
-             ' M ' + r + ' 0 h ' + (-l) +  
-             ' M 0 ' + r + ' v ' + (-l);
-    },
-    'cross-circle': function(size, ratio) {
-      var s = Math.sqrt(size), 
-          r = s/2;
-      return 'M' + (-r) + ',' + (-r) +
-      ' m' + (-r) + ',0' +
-      ' a' + r + ',' + r + ' 0 1,0' + (r * 2) + ',0' +
-      ' a' + r + ',' + r + ' 0 1,0' + (-(r * 2)) + ',0' +
-      ' M' + (-r) + ' 0 h ' + (s) + 
-      ' M 0 ' + (-r) + ' v ' + (s);
-          
-    },
-    'stroke-circle': function(size, ratio) {
-      var s = Math.sqrt(size), 
-          r = s/2;
-      return 'M' + (-r) + ',' + (-r) +
-      ' m' + (-r) + ',0' +
-      ' a' + r + ',' + r + ' 0 1,0' + (r * 2) + ',0' +
-      ' a' + r + ',' + r + ' 0 1,0' + (-(r * 2)) + ',0' +
-      ' M' + (-s-2) + ',' + (-s-2) + ' l' + (s+4) + ',' + (s+4);
+var customSvgSymbols = d3.map({
+  'ellipse': function(size, ratio) {
+    var s = Math.sqrt(size), 
+        rx = s*0.666, ry = s/3;
+    return 'M' + (-rx) + ',' + (-ry) +
+    ' m' + (-rx) + ',0' +
+    ' a' + rx + ',' + ry + ' 0 1,0' + (rx * 2) + ',0' +
+    ' a' + rx + ',' + ry + ' 0 1,0' + (-(rx * 2)) + ',0';
+  },
+  'marker': function(size, ratio) {
+    var s =  size > 48 ? size / 4 : 12,
+        r = s/2, l = r-3;
+    return 'M ' + (-r) + ' 0 h ' + l + 
+           ' M 0 ' + (-r) + ' v ' + l + 
+           ' M ' + r + ' 0 h ' + (-l) +  
+           ' M 0 ' + r + ' v ' + (-l);
+  },
+  'cross-circle': function(size, ratio) {
+    var s = Math.sqrt(size), 
+        r = s/2;
+    return 'M' + (-r) + ',' + (-r) +
+    ' m' + (-r) + ',0' +
+    ' a' + r + ',' + r + ' 0 1,0' + (r * 2) + ',0' +
+    ' a' + r + ',' + r + ' 0 1,0' + (-(r * 2)) + ',0' +
+    ' M' + (-r) + ' 0 h ' + (s) + 
+    ' M 0 ' + (-r) + ' v ' + (s);
+        
+  },
+  'stroke-circle': function(size, ratio) {
+    var s = Math.sqrt(size), 
+        r = s/2;
+    return 'M' + (-r) + ',' + (-r) +
+    ' m' + (-r) + ',0' +
+    ' a' + r + ',' + r + ' 0 1,0' + (r * 2) + ',0' +
+    ' a' + r + ',' + r + ' 0 1,0' + (-(r * 2)) + ',0' +
+    ' M' + (-s-2) + ',' + (-s-2) + ' l' + (s+4) + ',' + (s+4);
 
-    }, 
-    "crescent": function(size, ratio) {
-      var s = Math.sqrt(size), 
-          r = s/2,
-          ph = 0.5 * (1 - Math.cos(ratio)),
-          e = 1.6 * Math.abs(ph - 0.5) + 0.01,
-          dir = ratio > Math.PI ? 0 : 1,
-          termdir = Math.abs(ph) > 0.5 ? dir : Math.abs(dir-1); 
-      return 'M ' + (-1) + ',' + (-1) +
-      ' m 1,' + (-r+1) + 
-      ' a' + r + ',' + r + ' 0 1 ' + dir + ' 0,' + (r * 2) +
-      ' a' + (r*e) + ',' + r + ' 0 1 ' + termdir + ' 0,' + (-(r * 2)) + 'z';
-    } 
-  });
+  }, 
+  "crescent": function(size, ratio) {
+    var s = Math.sqrt(size), 
+        r = s/2,
+        ph = 0.5 * (1 - Math.cos(ratio)),
+        e = 1.6 * Math.abs(ph - 0.5) + 0.01,
+        dir = ratio > Math.PI ? 0 : 1,
+        termdir = Math.abs(ph) > 0.5 ? dir : Math.abs(dir-1); 
+    return 'M ' + (-1) + ',' + (-1) +
+    ' m 1,' + (-r+1) + 
+    ' a' + r + ',' + r + ' 0 1 ' + dir + ' 0,' + (r * 2) +
+    ' a' + (r*e) + ',' + r + ' 0 1 ' + termdir + ' 0,' + (-(r * 2)) + 'z';
+  } 
+});
 
-  d3.svg.customSymbol = function() {
-    var type, size = 64, ratio = d3.functor(1);
-    
-    function symbol(d,i) {
-      return customSvgSymbols.get(type.call(this,d,i))(size.call(this,d,i), ratio.call(this,d,i));
-    }
-    symbol.type = function(_) {
-      if (!arguments.length) return type; 
-      type = d3.functor(_);
-      return symbol;
-    };
-    symbol.size = function(_) {
-      if (!arguments.length) return size; 
-      size = d3.functor(_);
-      return symbol;
-    };
-    symbol.ratio = function(_) {
-      if (!arguments.length) return ratio; 
-      ratio = d3.functor(_);
-      return symbol;
-    };
+d3.svg.customSymbol = function() {
+  var type, size = 64, ratio = d3.functor(1);
+  
+  function symbol(d,i) {
+    return customSvgSymbols.get(type.call(this,d,i))(size.call(this,d,i), ratio.call(this,d,i));
+  }
+  symbol.type = function(_) {
+    if (!arguments.length) return type; 
+    type = d3.functor(_);
     return symbol;
   };
+  symbol.size = function(_) {
+    if (!arguments.length) return size; 
+    size = d3.functor(_);
+    return symbol;
+  };
+  symbol.ratio = function(_) {
+    if (!arguments.length) return ratio; 
+    ratio = d3.functor(_);
+    return symbol;
+  };
+  return symbol;
+};
 
 var datetimepicker = function(cfg, callback) {
   var date = new Date(), 
