3fc8c3c35c097270400f2f35e75b4db5acacf0db
kgoyal000 <62390711+kgoyal000@users.noreply.github.com>
2020-06-21 18:35:00 +0530

    Update celestial.js

--- celestial.js
+++ celestial.js
@@ -3402,9 +3402,15 @@ function geo(cfg) {
   
   function setPosition(p, settime) {
     if (!p || !has(config, "settimezone") || config.settimezone === false) return;
-    var timestamp = Math.floor(date.getTime() / 1000),
-        url = "http://api.timezonedb.com/v2.1/get-time-zone?key=" + config.timezoneid + "&format=json&by=position" + 
+	  
+    var timestamp = Math.floor(date.getTime() / 1000);
+	  if(window.location.protocol !== 'https:'){
+       var url = "http://api.timezonedb.com/v2.1/get-time-zone?key=" + config.timezoneid + "&format=json&by=position" + 
               "&lat=" + p[0] + "&lng=" + p[1] + "&time=" + timestamp;
+	  }else{
+		  var url = "https://api.timezonedb.com/v2.1/get-time-zone?key=" + config.timezoneid + "&format=json&by=position" + 
+              "&lat=" + p[0] + "&lng=" + p[1] + "&time=" + timestamp;
+	  }
        // oldZone = timeZone;
 
     d3.json(url, function(error, json) { 
